3915105bb578998f7b0034b738c351c8e3b2a1d8
hokomoko <kaan.yildiz@protonmail.com>
2022-10-08 03:42:29 +0300

    ActiveJ - Basic async http servlet added

---
+++ activej-app/src/main/java/com/itwasneo/activejapp/Application.java
@@ -0,0 +1,28 @@
+package com.itwasneo.activejapp;
+
+import io.activej.http.AsyncServlet;
+import io.activej.http.HttpMethod;
+import io.activej.http.HttpResponse;
+import io.activej.http.RoutingServlet;
+import io.activej.inject.annotation.Provides;
+import io.activej.launcher.Launcher;
+import io.activej.launchers.http.HttpServerLauncher;
+
+public class Application extends HttpServerLauncher {
+
+	@Provides
+	AsyncServlet servlet() {
+
+		return RoutingServlet.create()
+				.map(HttpMethod.GET, "/hi", req -> {
+					return HttpResponse.ok200().withPlainText("Hi");
+				});
+	}
+
+
+
+	public static void main(String[] args) throws Exception {
+		Launcher launcher = new Application();
+		launcher.launch(args);
+	}
+}
